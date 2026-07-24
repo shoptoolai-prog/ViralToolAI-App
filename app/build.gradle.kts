ef5594499794679c5fa00c0588c5aeb347067bdd
shoptoolai-prog <asityadavteambusiness@gmail.com>
2026-07-24 19:25:18 +0530

    Downgrade compileSdk and targetSdk to version 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,12 +11,13 @@ plugins {
 
 android {
   namespace = "com.example"
-  compileSdk { version = release(36) { minorApiLevel = 1 } }
+
+  compileSdk = 35
 
   defaultConfig {
     applicationId = "com.aistudio.shoptoolai.fpxwkr"
     minSdk = 24
-    targetSdk = 36
+    targetSdk = 35
     versionCode = 1
     versionName = "1.0.0"
 
